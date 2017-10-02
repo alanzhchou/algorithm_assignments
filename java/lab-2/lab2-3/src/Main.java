0d214d82847c50415051fef33ea65805e4a17364
alanzhchou <13713891649@163.com>
2017-10-02 22:08:22 +0800

    2017.10.02

--- java/lab-2/lab2-3/src/Main.java
+++ java/lab-2/lab2-3/src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
     }
+}
+
+
+class twin{
+    private int[] findTwin;
+    private int key;
+    private int number = 0;
+
+    public twin(){
+
+    }
+
 }
