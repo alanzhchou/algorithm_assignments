f3752a08eff787c3814b38c3ab377f064bb9e810
alanzhchou <13713891649@163.com>
2017-10-12 23:10:28 +0800

    Revert "2017.10.11"
    This reverts commit 27728f4708cfdbfbad812f4e0f2a5489f157d0e5.

--- java/java2-lab3/src/Main.java
+++ java/java2-lab3/src/Main.java
@@ -1,11 +1,11 @@
 import java.io.File;
 import java.io.IOException;
 
-public class one {
+public class Main {
 
-//    public static void main(String[] args) throws IOException{
-//        find("A:\\QSunSync", depth);
-//    }
+    public static void main(String[] args) throws IOException{
+        find("A:\\QSunSync", depth);
+    }
 
     private static int depth=1;
 
