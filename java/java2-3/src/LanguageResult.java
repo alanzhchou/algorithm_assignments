648b2fce6839a6418e8cff1864081e5abc7f2b97
alanzhchou <13713891649@163.com>
2017-10-13 12:13:11 +0800

    2017.10.13

--- java/java2-3/src/LanguageResult.java
+++ java/java2-3/src/LanguageResult.java
@@ -7,17 +7,12 @@ import java.util.stream.Stream;
 /**
  * Created by ZH-AlanChou on 2017/10/13.
  */
-public class Languages {
+public class LanguageResult {
     public static void  main(String[] args) throws IOException {
         ReadCnf cnf = new ReadCnf();
 
         PathMapping mapping = new PathMapping();
         mapping.getPathMapping(cnf.getStop_words_dir());
         mapping.toString();
-
-
-
-
-
     }
 }
