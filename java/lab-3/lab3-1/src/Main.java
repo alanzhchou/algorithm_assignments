5b1cc4df1bb947a1d1d4c21860e7051db1c80a05
alanzhchou <13713891649@163.com>
2017-10-15 01:06:37 +0800

    2017.10.15
    数据结构lab3-1完成

--- java/lab-3/lab3-1/src/Main.java
+++ java/lab-3/lab3-1/src/Main.java
@@ -1,6 +1,58 @@
-public class Main {
+import java.util.LinkedList;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] A = {1,2,3};
+        int[] B = {1,2,3,4,5};
+
+        Conbin result = new Conbin(A,B);
+        printArr(result.getSorted());
+
+        int[] C = {1,1,2,2};
+        int[] D = {1,1,2,2,3,3,4};
+        Conbin result2 = new Conbin(C,D);
+        printArr(result2.getSorted());
+    }
+
+    static void printArr(int[] a){
+        for (int i=0; i<a.length; i++){
+            System.out.print(a[i] + "\t");
+        }
+        System.out.println();
+    }
+}
+
+class Conbin{
+    private int[] A;
+    private int[] B;
+    private int[] C;
+
+    public Conbin(int[] a, int[] b){
+        this.A = a;
+        this.B = b;
+        this.C = new int[A.length + B.length];
+    }
+
+    public int[] getSorted(){
+        int indexA = 0;
+        int indexB = 0;
+        int indexC = 0;
+
+        while (indexA < A.length&&indexB < B.length){
+            if (A[indexA] <= B[indexB]){
+                C[indexC++] = A[indexA++];
+            }else {
+                C[indexC++] = B[indexB++];
+            }
+        }
+
+        while (indexA < A.length) {
+            C[indexC++] = A[indexA++];
+        }
+        while (indexB < B.length) {
+            C[indexC++] = B[indexB++];
+        }
+
+        return C;
     }
 }
